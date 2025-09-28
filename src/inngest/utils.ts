111aacf850c80a3096b0a06ff6ef757e08f0efcd
ASHUTOSH-KUMAR-RAO <icoashutosh@gmail.com>
2025-09-28 14:43:16 +0530

    07:agent-tool

--- src/inngest/utils.ts
+++ src/inngest/utils.ts
@@ -1,4 +1,5 @@
 import { Sandbox } from "@e2b/code-interpreter";
+import { AgentResult, TextMessage } from "@inngest/agent-kit";
 
 export async function getSandbox(sandboxId: string) {
   // Logic to retrieve the sandbox URL based on the sandboxId
@@ -6,4 +7,20 @@ export async function getSandbox(sandboxId: string) {
   const sandbox = await Sandbox.connect(sandboxId);
 
   return sandbox;
+}
+
+export function lastAssistantTextMessageContent(result: AgentResult) {
+  const lastAssistantTextMessageContent = result.output.findLastIndex(
+    (msg) => msg.role === "assistant" && msg.type === "text"
+  );
+
+  const message = result.output[lastAssistantTextMessageContent] as
+    | TextMessage
+    | undefined;
+
+  return message?.content
+    ? typeof message.content === "string"
+      ? message.content
+      : message.content.map((c) => c.text).join("")
+    : undefined;
 }
